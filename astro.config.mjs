5bd141124b0efc49f65b840346f113f20d63d0b1
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-03-13 22:26:42 +1100

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://<YOUR_GITHUB_USERNAME>.github.io",
+  base: "/<YOUR_REPOSITORY_NAME>",
+});
