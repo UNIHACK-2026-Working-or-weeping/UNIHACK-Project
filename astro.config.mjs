34c0119d2caf93f4ce0426100f7c0d2471783a2e
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-03-13 22:44:41 +1100

    Redid foundation with tailwind and shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://unihack-2026-working-or-weeping.github.io",
-  base: "/UNIHACK-Project",
-});
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react()],
+})
