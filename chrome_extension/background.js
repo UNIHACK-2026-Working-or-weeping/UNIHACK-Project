0572fa95de4233c464c5b0b9aa38179936301407
plumzoe <zhengzoe718@gmail.com>
2026-03-14 16:26:45 +1100

    Remove default domains initialization on install
    Removed the onInstalled listener that initializes default domains in local storage.

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -13,13 +13,7 @@ const defaultDomains = [
   "youtube.com",
   "twitch.tv",
 ];
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.get("defaultDomains", function(result) {
-    if (!result.defaultDomains) { 
-      chrome.storage.local.set({ defaultDomains });
-    }
-  });
-});
+
 
 let customDomains = [];
 let previousTabId = null;
